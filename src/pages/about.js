a8a9c4b6b3ebafa38e75860915334c25c0857d82
clorefi <89710354+clorefi@users.noreply.github.com>
2021-09-01 22:05:05 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,7 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle='About Me'>
-      <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
