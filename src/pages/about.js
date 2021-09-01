4e07d36b62cc81c50b42d94b6b4ec5383ac9b09a
clorefi <89710354+clorefi@users.noreply.github.com>
2021-09-01 22:02:35 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
+    <Layout pageTitle='About Me'>
       <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    </Layout>
   )
 }
 
