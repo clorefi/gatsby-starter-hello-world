6414db2d8f1c895290f38285ae14432ad7b343b9
clorefi <89710354+clorefi@users.noreply.github.com>
2021-09-01 22:04:10 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout titlePage='Home Page'>
+    <Layout pageTitle='Home Page'>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
