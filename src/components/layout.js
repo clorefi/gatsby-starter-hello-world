25f74589b769a36da3713f27576a402252e30ba6
clorefi <89710354+clorefi@users.noreply.github.com>
2021-09-01 22:17:47 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
+
 
 
 const Layout = ({ pageTitle, children }) => {
@@ -8,13 +15,20 @@ const Layout = ({ pageTitle, children }) => {
     <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li> 
+            <Link to="/" className={navLinkText}>
+            Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={navLinkText}>
+              About</Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
